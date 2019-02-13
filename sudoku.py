aadadc0e6110b82fa35ce2c20954f5e7247d61c5
Ivette <car16020@uvg.edu.gt>
2019-02-13 08:22:56 -0600

    matriz

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,34 @@
-#UVG
-#Ivette Cardona - 16020
-#sudoku.py
+"""
+    UVG
+    Ivette Cardona - 16020
+    sudoku.py
+    Style: PEP8  -   https://www.python.org/dev/peps/pep-0008/
+"""
 
-#Ingresar problema
-entrada = input("Ingrese sudoku ")
+import numpy as np
 
-print ("Sudoku a resolver: ", entrada)
+blank = 0
+space = " "
+line = "|"
+row = "-------------------------------------"
+
+dim = int(input("¿Dimensiones del sudoku? "))
+#Inicializar matriz de dimxdim con ceros
+mat = np.zeros ((dim,dim))
+
+problem = input("Ingrese el sudoku que desea resolver: ")
+
+#Llenando matriz 
+for i in range(dim): 
+    if(problem[i] == "."):
+        mat[i] = 0
+    else: 
+        mat[i] = (int)(problem[i])
+
+print(mat)
+
+"""
+def show_grid(int grid[dim][dim]):
+    for i in range(0,dim):
+        print(blank)
+"""
