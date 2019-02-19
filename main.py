c0da6c176087499b45dd4a4453c2ad60fecd2596
Ivette <car16020@uvg.edu.gt>
2019-02-19 14:36:55 -0600

    entry

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+    UVG
+    Ivette Cardona - 16020
+    main.py
+    Style: PEP8  -   https://www.python.org/dev/peps/pep-0008/
+"""
+
--- puzzle.py
+++ puzzle.py
@@ -9,12 +9,19 @@ import ast
 import math
 import numpy as np 
 
+GOAL = [
+    '1','2','3','4',
+    '5','6','7','8',
+    '9','A','B','C',
+    'D','E','F','_'
+]
+
 class Node:
-    def __init__(self,data,level,fvalue):
+    def __init__(self,data,level,fval):
         self.data = data
         self.level = level
-        self.fvalue = fvalue
-    
+        self.fval = fval
+
     def movement(self, puzzle, x1, x2, y1, y2): 
         if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
             temp_puz = []
@@ -25,3 +32,24 @@ class Node:
             return temp_puz
         else:
             return None
+
+class Puzzle: 
+    def __init__(self):
+        self.open = []
+        self.closed = []
+
+    def entry(self):
+        """ Entrada de usuario """
+        puz: []
+        temp = input()
+        puz = list(temp)
+
+        print(puz) 
+        return puz
+
+    def process(self):
+        print("Entre el 15p a resolver: \n")
+        start = self.entry()
+
+fifteen = Puzzle()
+fifteen.process()
