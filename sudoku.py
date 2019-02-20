74ad1a391dc76d4b23535823e3f4f6091debeb22
Ivette <car16020@uvg.edu.gt>
2019-02-20 00:20:14 -0600

    sudoku actions

--- sudoku.py
+++ sudoku.py
@@ -14,6 +14,7 @@ SPACE = " "
 LINE = "|"
 ROW = "-------------------------------------"
 NUMBERS = ["1", "2", "3", "4"]
+DIM = 4
 
 class Start: 
     def entry(self):
@@ -33,18 +34,55 @@ class Start:
         for i in input: 
             row.append(i)
             n += 1 
-            if (n%4 == 0):
+            if (n%DIM == 0):
                 sudoku.append(row)
                 row = [] #clean row
         return sudoku
+    
+    def grid(self, matrix):
+        pass
 
 class Sudoku: 
 
     initial = "Initial state"
 
     def actions(self, matrix): 
-        #rules 
-        pass
+        empty = 0
+        n = 0
+        m = 0
+        position = []
+        possibility = []
+        poss_matrix = []
+
+        #Count of empty spaces
+        for x in range(DIM):
+            for y in range(DIM): 
+                if matrix[x][y] == ".": 
+                    position.append(tuple([x,y]))
+                    empty += 1
+
+        num = NUMBERS
+        while(n < empty): 
+            for i in range(DIM): 
+                if ((matrix[position[n][0]][i] != ".") and (matrix[position[n][0]][i] in num)): 
+                    num.remove(matrix[position[n][0]][i])
+                    
+            for i in range(DIM):
+                 if ((matrix[i][position[n][1]] != ".") and (matrix[i][position[n][1]] in num)): 
+                    num.remove(matrix[i][position[n][1]])
+            
+            possibility.append(num)
+            n += 1
+        
+        while(m < empty): 
+            for i in possibility[m]: 
+                temp = copy.deepcopy(matrix)
+                temp[position[m][0]][position[m][1]] = i
+                poss_matrix.append(temp)
+            m += 1
+
+        return poss_matrix 
+
     
     def result(self):
         pass
