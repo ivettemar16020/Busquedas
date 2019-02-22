66e26bdab217869e9fe0ee2645f6c32573f46a26
Ivette <car16020@uvg.edu.gt>
2019-02-21 18:04:30 -0600

    final

--- puzzle.py
+++ puzzle.py
@@ -41,58 +41,13 @@ class Start:
             if (response == False):
                 print(response)
             break
-"""
-    def create_matrix(self, dim, arr):
-        matrix = np.zeros((dim,dim))
-        arrval = 0
-        for x in range(dim):
-            for y in range (dim):
-                matrix[x,y] = arr[arrval]
-                arrval = arrval +1
-        return matrix
 
-    def blanknode(self, matrix, n, val):
-        n = Node()
-        for x in range(n):
-            for y in range (n):
-                value = matrix[x,y]
-                if value == val:
-                    return n.node(x,y)
-
-class Node():
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-"""
 class Puzzle: 
     init = "Initial state"
 
-    start = Start()
-"""
-    n = Node(0,0)
 
-    def newFrontier(self, s,a):   
-        vacio = start.blanknode(s,4,0)
-        if(a == "arriba"):
-            return n.node(vacio.x-1,vacio.y)                        
-        if(a == "abajo"):
-            return n.node(vacio.x+1,vacio.y)         
-        if(a == "izquierda"):
-            return n.node(vacio.x,vacio.y-1)       
-        if(a == "derecha"):
-            return n.node(vacio.x,vacio.y+1) 
-
-    def goal_test(self, dim): 
-        matrix = np.zeros((dim,dim))
-        arrval = 1
-        for x in range(dim):
-            for y in range (dim):
-                if(y == dim-1 and x == dim-1):
-                    matrix[x,y] = 0
-                else:
-                    matrix[x,y] = arrval
-                    arrval = arrval +1
-        return matrix
+    def goal_test(self, matrix): 
+        pass
 
     def process(self): 
-        pass"""
+        pass
